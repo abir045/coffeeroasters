ad6556fcbebb5f4c8f5e08887fd53ab4730c56ca
Salekin Imran <abir045@gmail.com>
2022-06-02 00:07:50 +0600

    desktop responsive layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,9 @@ const Navbar = () => {
           <h3 className="font-bold text-3xl">coffeeroasters</h3>
         </div>
 
-        <GiHamburgerMenu size={40} className="mx-auto mr-8" />
+        <GiHamburgerMenu size={40} className="mx-auto mr-8 sm:hidden" />
 
-        <div className="hidden sm:flex p-8 font-medium  text-gray-900">
+        <div className="hidden sm:flex p-8 font-medium  text-gray-900 sm:mr-[5%]">
           <div className="text-xs  uppercase sm:mx-1">Home</div>
           <div className="text-xs uppercase sm:mx-1">About Us</div>
           <div className="text-xs uppercase sm:mx-1">Create your plan</div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -29,8 +29,8 @@ const Home = () => {
         />
         <div className="absolute top-0 left-0 w-full h-full bg-black/80 opacity-70  text-white"></div>
 
-        <div className="absolute left-[3%] top-[15%] md:left-8 md:top-35 lg:top-40">
-          <h1 className="text-center text-white p-4 font-bold text-5xl sm:text-left  md:text-6xl  w-full  xl:w-1/3  md:w-1/2 sm:w-2/3">
+        <div className="absolute left-[3%] top-[15%] md:left-8 md:top-35 lg:top-[10%] lg:left-[5%]">
+          <h1 className="text-center text-white p-4 font-bold text-5xl sm:text-left  md:text-5xl lg:text-6xl w-full lg:w-1/2 xl:w-1/3  md:w-1/2 sm:w-2/3">
             Great coffee made simple.
           </h1>
           <p className="text-xl text-center md:text-left sm:text-left  text-gray-300 p-4 tracking-wide w-full leading-10 xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-4/5 ">
@@ -39,7 +39,7 @@ const Home = () => {
             your door, at your schedule
           </p>
 
-          <button className="text-white mx-16  text-3xl bg-teal-700 font-bold rounded-lg p-6 my-10 sm:mx-4">
+          <button className="text-white mx-14  text-3xl bg-teal-700 font-bold rounded-lg p-4 my-10 sm:mx-4">
             Create your plan
           </button>
         </div>
@@ -47,22 +47,22 @@ const Home = () => {
 
       {/*Menu conatainer  */}
       <div className="block">
-        <h1 className="mt-4 text-center text-9xl font-bold sm:tracking-[.15em] text-gray-300">
+        <h1 className="mt-4 text-center text-9xl font-bold sm:text-7xl sm:tracking-[.1em] text-gray-300">
           our collection
         </h1>
       </div>
 
       <div className="flex flex-col p-8 items-center  md:flex-row md:justify-evenly md:-mt-12 md:pt-1">
-        <img className="w-[250px]" src={CoffeBag} alt="menu" />
+        <img className="w-[150px]" src={CoffeBag} alt="menu" />
 
         {/* 2nd item */}
-        <img className="w-[250px]  " src={Planalto} alt="menu" />
+        <img className="w-[150px]  " src={Planalto} alt="menu" />
 
         {/* 3rd item */}
-        <img className="w-[250px] " src={Piccolo} alt="menu" />
+        <img className="w-[150px] " src={Piccolo} alt="menu" />
 
         {/* 4th item */}
-        <img className="w-[250px] " src={Danche} alt="menu" />
+        <img className="w-[150px] " src={Danche} alt="menu" />
       </div>
 
       <div className="flex flex-col p-8  md:flex-row md:justify-evenly">
@@ -70,7 +70,7 @@ const Home = () => {
           <h3 className="text-4xl text-center font-bold text-gray-800 ">
             Gran Espresso
           </h3>
-          <p className="text-xl text-center m-2 p-2   leading-10 text-gray-500">
+          <p className="text-lg text-center m-4 p-4   leading-10 text-gray-500">
             Light and flavorful blend with cocoa and black pepper for an intense
             exoperience
           </p>
@@ -80,7 +80,7 @@ const Home = () => {
           <h3 className="text-center text-4xl font-bold text-gray-800">
             Planalto
           </h3>
-          <p className="text-center text-xl p-2 m-2 leading-10 text-gray-500">
+          <p className="text-center text-lg p-2 m-2 leading-10 text-gray-500">
             Brazilian dark roast with rich velvety and body, and rich with
             fruits and nuts
           </p>
@@ -90,7 +90,7 @@ const Home = () => {
           <h3 className="text-4xl text-center font-bold text-gray-800">
             Piccolo
           </h3>
-          <p className="text-center text-xl text-center p-4 leading-10 text-gray-500">
+          <p className="text-lg text-center p-4 leading-10 text-gray-500">
             Mild and smooth blend featuring notes of toasted almond and dried
             cherry
           </p>
@@ -99,7 +99,7 @@ const Home = () => {
           <h3 className="text-4xl text-center font-bold text-gray-800">
             Danche
           </h3>
-          <p className="text-center text-xl p-2 m-2 leading-10 text-gray-500">
+          <p className="text-center text-lg p-2 m-2 leading-10 text-gray-500">
             Ethiopian hand-hervested blend densly packed with vibrant fruit
             notes
           </p>
