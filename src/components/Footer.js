e5d2eab55e5615e198c7b1db4431b30ebd323551
Salekin Imran <abir045@gmail.com>
2022-07-28 14:53:28 +0600

    fully responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,32 +8,37 @@ import Instagram from "../images/instagram.svg";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between w-full bg-gray-900">
-      <div className="flex p-6 sm:p-8 justify-between ">
+    <div className="flex flex-col items-center justify-between w-full bg-gray-800 pt-10  sm:flex-col md:flex-row">
+      <div className="flex p-4 sm:p-8 justify-between ">
         <img className="w-[30px] -rotate-90 mx-1" src={Coffee} alt="coffee" />
         <h3 className="font-bold text-3xl text-white">coffeeroasters</h3>
       </div>
 
       {/* <GiHamburgerMenu size={40} className="mx-auto mr-8 sm:hidden" /> */}
 
-      <div className="hidden sm:flex  p-8 font-medium  text-gray-900 sm:mr-[5%]">
-        <a
-          className="text-xs  cursor-pointer uppercase sm:mx-1 text-white"
-          href="/"
-        >
+      <div className="flex flex-col text-gray-400 space-y-6 text-center mt-5 sm:hidden">
+        <a className="text-xs  cursor-pointer uppercase   " href="/">
           Home
         </a>
-        <a
-          className="text-xs cursor-pointer uppercase sm:mx-1 text-white"
-          href="/about"
-        >
+        <a className="text-xs cursor-pointer uppercase  " href="/about">
           About Us
         </a>
-        <a className="text-xs cursor-pointer uppercase sm:mx-1 text-white">
+        <a className="text-xs cursor-pointer uppercase  ">Create your plan</a>
+      </div>
+      <div className="hidden  sm:flex space-x-10 p-4 font-medium  text-gray-400 sm:mr-[5%]">
+        {/* for mobile */}
+
+        <a className="text-xs  cursor-pointer uppercase sm:mx-1  " href="/">
+          Home
+        </a>
+        <a className="text-xs cursor-pointer uppercase sm:mx-1 " href="/about">
+          About Us
+        </a>
+        <a className="text-xs cursor-pointer uppercase sm:mx-1 ">
           Create your plan
         </a>
       </div>
-      <div className="flex  space-x-3 mr-10">
+      <div className="flex justify-center items-center p-6  mt-5 mb-10 space-x-3 ">
         <img className="bg-white w-[30px]" src={Facebook} />
         <img className="bg-white w-[30px]" src={Twitter} />
         <img className="bg-white w-[30px]" src={Instagram} />
